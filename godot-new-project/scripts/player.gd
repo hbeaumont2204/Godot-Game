ef24c09ef7c7efefb6293180a024c14f392b81e9
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2026-02-07 20:07:05 +0000

    Script update

--- godot-new-project/scripts/player.gd
+++ godot-new-project/scripts/player.gd
@@ -4,7 +4,8 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 const life_force_max = 180
-var life_force = 180
+var life_force = 10 # Change for testing, default is 60
+var is_dead = false
 
 var time = 0.0 # Time taken by the player
 
@@ -17,15 +18,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	#print(life_force) # TEST
 	life_force_drain(delta)
+	
+	
 
 func restore_life_force() -> void:
 	life_force = life_force_max # Resets to full
 
 func life_force_drain(delta: float) -> void:
 	life_force = life_force - delta
-	#if life_force <= 0:
+	if life_force <= 0:
 		# Game over
-	
+		is_dead = true
+	if is_dead:
+		get_tree().paused = true
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
