f190d39385d36e04f96a9d54b6ffe634b1526de7
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2026-03-04 19:10:53 +0000

    BACKUP

--- godot-new-project/scenes/enemies/enemy.gd
+++ godot-new-project/scenes/enemies/enemy.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-#@onready var timer = $Timer
 @onready var player = get_parent().get_node("Player")
 
 # Find alternative to on body entered later
@@ -11,6 +10,5 @@ func _on_body_entered(body: Node2D) -> void:
 		
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("projectile"):
-		print("Projectile")
 		area.queue_free()
 		get_parent().queue_free() # delete slime
